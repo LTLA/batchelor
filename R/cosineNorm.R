334a808f74e806d8bcf593cb3f8e6afe4999b704
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-11-05 16:08:52 +0000

    Added cosine normalization R code.

---
+++ R/cosineNorm.R
@@ -0,0 +1,42 @@
+#' Cosine normalization
+#'
+#' Perform cosine-normalization on the column vectors of an expression matrix.
+#'    
+#' @param x A gene expression matrix with cells as columns and genes as rows.
+#' @param mode A string specifying the output to be returned.
+#'
+#' @details
+#' While the default is to directly return the cosine-normalized matrix, it may occasionally be desirable to obtain the L2 norm, 
+#' e.g., to apply an equivalent normalization to other matrices.
+#' This can be achieved by setting \code{mode} accordingly.
+#'
+#' @return
+#' If \code{mode="matrix"}, a double-precision matrix of the same dimensions as \code{X} is returned, containing cosine-normalized values.
+#' 
+#' If \code{mode="l2norm"}, a double-precision vector is returned containing the L2 norm for each cell.
+#' 
+#' If \code{mode="all"}, a named list is returned containing the fields \code{"matrix"} and \code{"l2norm"}, which are as described above.
+#'
+#' @author
+#' Aaron Lun
+#' 
+#' @seealso
+#' \code{\link{mnnCorrect}} and \code{\link{fastMNN}}, where this function gets used.
+#'
+#' @examples
+#' A <- matrix(rnorm(1000), nrow=10)
+#' str(cosineNorm(A))
+#' str(cosineNorm(A, mode="l2norm"))
+#' 
+#' @export
+cosineNorm <- function(X, mode=c("matrix", "all", "l2norm"))
+# Computes the cosine norm, with some protection from zero-length norms.
+#
+# written by Aaron Lun
+# 5 July 2018
+{
+    mode <- match.arg(mode)
+    out <- .Call(cxx_cosine_norm, X, mode!="l2norm")
+    names(out) <- c("matrix", "l2norm")
+    switch(mode, all=out, matrix=out$matrix, l2norm=out$l2norm)
+}
