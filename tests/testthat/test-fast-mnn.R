4381d926d1439d608fc9ab578acfd7af2f37e475
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-11-06 10:36:29 +0000

    Updated fastMNN tests for new output.

--- tests/testthat/test-fast-mnn.R
+++ tests/testthat/test-fast-mnn.R
@@ -73,10 +73,13 @@ test_that("tricube weighting works correctly", {
     expect_equal(ref, out)
 })
 
-CHECK_PAIRINGS <- function(mnn.out) {
+CHECK_PAIRINGS <- function(mnn.out) 
+# Checks that the reported pairs belong to the same batch at each merge step,
+# and are consistent with the reported merge order.
+{
     origin <- as.vector(mnn.out$batch)
-    pairings <- mnn.out$pairs
-    order <- mnn.out$order
+    pairings <- metadata(mnn.out)$pairs
+    order <- metadata(mnn.out)$order
 
     sofar <- unique(origin[pairings[[1]]$first])
     expect_identical(sofar, order[1L])
@@ -110,7 +113,7 @@ test_that("fastMNN works as expected for two batches", {
     out <- fastMNN(B1, B2, d=50) # corrected values
     expect_identical(dim(out$corrected), c(ncol(B1) + ncol(B2), 50L))
     expect_identical(as.integer(out$batch), rep(1:2, c(ncol(B1), ncol(B2))))
-    expect_identical(out$order, 1:2)
+    expect_identical(metadata(out)$order, 1:2)
     CHECK_PAIRINGS(out)
     
     # Dimension choice behaves correctly.
@@ -140,7 +143,10 @@ test_that("fastMNN works as expected for two batches", {
     pcs <- multiBatchPCA(B1, B2, d=10)
     out.pre <- fastMNN(pcs[[1]], pcs[[2]], pc.input=TRUE)
     out.norm <- fastMNN(B1, B2, d=10, cos.norm=FALSE)
-    expect_equal(out.pre, out.norm[setdiff(names(out.norm), "rotation")])
+
+    expect_equal(metadata(pcs)$rotation, metadata(out.norm)$rotation)
+    metadata(out.norm)$rotation <- NULL
+    expect_equal(out.pre, out.norm)
 })
 
 set.seed(1200004)
@@ -160,11 +166,11 @@ test_that("variance loss calculations work as expected", {
     out3 <- batchelor:::.compute_intra_var(rbind(PC1, PC1), PC2, list(PC1, PC2), c(1L, 1L, 2L))
     expect_identical(out3, out[c(1,1,2)])
 
-    # Checking that we comput something.
+    # Checking that we compute something.
     mnn.out <- fastMNN(PC1, PC2, pc.input=TRUE, compute.variances=TRUE)
-    expect_identical(length(mnn.out$lost.var), 2L)
+    expect_identical(length(metadata(mnn.out)$lost.var), 2L)
     mnn.out2 <- fastMNN(PC2, PC1, auto.order=2:1, pc.input=TRUE, compute.variances=TRUE)
-    expect_identical(mnn.out$lost.var, rev(mnn.out2$lost.var))
+    expect_identical(metadata(mnn.out)$lost.var, rev(metadata(mnn.out2)$lost.var)) # variance loss at first step should be symmetric.
 })
 
 set.seed(1200005)
@@ -176,7 +182,7 @@ test_that("fastMNN works as expected for three batches with re-ordering", {
     out <- fastMNN(B1, B2, B3, d=50) # corrected values
     expect_identical(dim(out$corrected), c(ncol(B1) + ncol(B2) + ncol(B3), 50L))
     expect_identical(as.integer(out$batch), rep(1:3, c(ncol(B1), ncol(B2), ncol(B3))))
-    expect_identical(out$order, 1:3)
+    expect_identical(metadata(out)$order, 1:3)
     CHECK_PAIRINGS(out)
     
     # Testing the re-ordering algorithms.
@@ -185,18 +191,20 @@ test_that("fastMNN works as expected for three batches with re-ordering", {
 
     back.to.original <- order(out.re$batch)
     expect_equal(out$corrected, out.re$corrected[back.to.original,])
-    expect_identical(out.re$order, c("B1", "B2", "B3"))
+    expect_identical(metadata(out.re)$order, c("B1", "B2", "B3"))
 
+    old.pairs <- metadata(out)$pairs
+    new.pairs <- metadata(out.re)$pairs
     for (i in seq_along(out$pairs)) {
-        expect_identical(out$pairs[[i]]$first, match(out.re$pairs[[i]]$first, back.to.original))
-        expect_identical(out$pairs[[i]]$second, match(out.re$pairs[[i]]$second, back.to.original))
+        expect_identical(old.pairs[[i]]$first, match(new.pairs[[i]]$first, back.to.original))
+        expect_identical(old.pairs[[i]]$second, match(new.pairs[[i]]$second, back.to.original))
     }
 
-    expect_equal(out$rotation, out.re$rotation)
+    expect_equal(metadata(out)$rotation, metadata(out.re)$rotation)
 })
 
 set.seed(1200006)
-test_that("fastMNN works as expected for three batches with re-ordering", {
+test_that("fastMNN works as expected for three batches with auto ordering", {
     B1 <- matrix(rnorm(10000), nrow=100) # Batch 1 
     B2 <- matrix(rnorm(20000), nrow=100) # Batch 2
     B3 <- matrix(rnorm(5000), nrow=100) # Batch 2
@@ -206,21 +214,21 @@ test_that("fastMNN works as expected for three batches with re-ordering", {
     out.auto <- fastMNN(B1, B2, B3, d=50, auto.order=TRUE) 
     expect_identical(dim(ref$corrected), dim(out.auto$corrected))
     expect_identical(out.auto$batch, ref$batch)
-    expect_identical(out.auto$order, c(2L, 1L, 3L)) # 3 should be last, with the fewest cells => fewest MNNs.
+    expect_identical(metadata(out.auto)$order, c(2L, 1L, 3L)) # 3 should be last, with the fewest cells => fewest MNNs.
     CHECK_PAIRINGS(out.auto)
 
-    out.re.auto <- fastMNN(B1, B2, B3, auto.order=out.auto$order)
+    out.re.auto <- fastMNN(B1, B2, B3, auto.order=metadata(out.auto)$order)
     expect_equal(out.auto, out.re.auto)
 
     # Auto-ordering consistently handles the BiocNeighborIndex class.
     expect_error(out.approx <- fastMNN(B1, B2, B3, auto.order=TRUE, BNPARAM=BiocNeighbors::AnnoyParam()), NA)
-    expect_identical(out.approx$order, out.auto$order)
+    expect_identical(metadata(out.approx)$order, metadata(out.auto)$order)
 
     # Testing the internal auto-ordering functions.
     fmerge <- batchelor:::.define_first_merge(list(t(B1), t(B2), t(B3)), k=20)   
     expect_identical(fmerge$first, 2L) # 2 is arbitrarily 'first', and 1 is arbitrarily 'second'; but 3 should never show up.
     expect_identical(fmerge$second, 1L)
-    expect_identical(fmerge$pairs, batchelor:::find.mutual.nn(t(B2), t(B1), k1=20, k2=20))
+    expect_identical(fmerge$pairs, findMutualNN(t(B2), t(B1), k1=20, k2=20))
 
     expect_identical(BiocNeighbors::findKNN(precomputed=fmerge$precomputed[[1]], k=5), BiocNeighbors::findKNN(t(B1), k=5)) # checking that the precomputations are correct.
     expect_identical(BiocNeighbors::findKNN(precomputed=fmerge$precomputed[[2]], k=10), BiocNeighbors::findKNN(t(B2), k=10))
@@ -228,7 +236,7 @@ test_that("fastMNN works as expected for three batches with re-ordering", {
 
     nmerge <- batchelor:::.define_next_merge(t(B1), list(t(B1), t(B2), t(B3)), processed=1L, precomputed=fmerge$precomputed, k=20)   
     expect_identical(nmerge$other, 2L) # 1 should have more MNNs with 2 than with 3.
-    expect_identical(nmerge$pairs, batchelor:::find.mutual.nn(t(B1), t(B2), k1=20, k2=20))
+    expect_identical(nmerge$pairs, findMutualNN(t(B1), t(B2), k1=20, k2=20))
 })
 
 set.seed(12000051)
@@ -257,12 +265,14 @@ test_that("fastMNN works on SingleCellExperiment inputs", {
     expect_equal(ref, out)
 
     # Handles reduced dimensional inputs correctly.
-    ref <- fastMNN(B1, B2, cos.norm=FALSE)
     blah <- multiBatchPCA(sce1, sce2, get.spikes=TRUE)
     reducedDim(sce1) <- blah[[1]]
     reducedDim(sce2) <- blah[[2]]
     out <- fastMNN(sce1, sce2, use.dimred=1)
-    expect_equal(ref[setdiff(names(ref), "rotation")], out)
+
+    ref <- fastMNN(B1, B2, cos.norm=FALSE)
+    metadata(ref)$rotation <- NULL
+    expect_equal(ref, out)
 })
 
 set.seed(12000052)
@@ -288,7 +298,7 @@ test_that("fastMNN works with within-object batches", {
 
     # Checking the pairings.
     for (x in seq_along(out$pairs)) {
-        curref <- ref$pairs[[x]]
+        curref <- metadata(ref)$pairs[[x]]
         curref[,1] <- match(curref[,1], shuffle)
         curref[,2] <- match(curref[,2], shuffle)
         curout <- out$pairs[[x]]
