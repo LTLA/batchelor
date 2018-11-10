421298d17243fb392bb2cf23df7edc6cb7e5935f
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-11-10 11:40:16 +0000

    Expanded cosineNorm tests.

--- tests/testthat/test-cos-norm.R
+++ tests/testthat/test-cos-norm.R
@@ -4,14 +4,58 @@
 set.seed(10001)
 test_that("Cosine normalization is correct", {
     X <- matrix(rnorm(10000), ncol=100)
-    cellnorm <- pmax(1e-8, sqrt(colSums(X^2)))
+    cellnorm <- sqrt(colSums(X^2))
     ref <- X/matrix(cellnorm, nrow(X), ncol(X), byrow=TRUE)
     expect_equal(ref, cosineNorm(X))
 
     X <- matrix(rpois(20000, lambda=5), ncol=1000)
-    cellnorm <- pmax(1e-8, sqrt(colSums(X^2)))
+    cellnorm <- sqrt(colSums(X^2))
     ref <- X/matrix(cellnorm, nrow(X), ncol(X), byrow=TRUE)
     expect_equal(ref, cosineNorm(X))
+
+    # Recovering both or either works.
+    out <- cosineNorm(X, mode="all")
+    expect_equal(out$matrix, ref)
+    expect_equal(out$l2norm, cellnorm)
+
+    out <- cosineNorm(X, mode="l2norm")
+    expect_equal(out, cellnorm)
+})
+
+set.seed(100011)
+test_that("Cosine normalization preserves sparsity", {
+    library(Matrix)
+    X <- rsparsematrix(100, 20, 0.2)
+    out <- cosineNorm(X)
+    expect_s4_class(out, "dgCMatrix")
+
+    ref <- cosineNorm(as.matrix(X))
+    expect_equivalent(ref, as.matrix(out))
+    
+    # We get the l2 norm correctly.
+    out <- cosineNorm(X, mode="l2norm")
+    ref <- cosineNorm(as.matrix(X), mode="l2norm")
+    expect_equivalent(ref, out)
+})
+
+test_that("Cosine normalization avoids division by zero", {
+    X <- matrix(0, 100, 20)
+    out <- cosineNorm(X, mode="all")
+    expect_equal(out$matrix, X)
+    expect_equal(out$l2norm, numeric(ncol(X)))
+})
+
+set.seed(100012)
+test_that("Cosine normalization preserves Delayed'ness", {
+    X <- DelayedArray(matrix(rnorm(2000), nrow=100))
+    out <- cosineNorm(X)
+    ref <- cosineNorm(as.matrix(X))
+    expect_equal(as.matrix(out), ref)
+
+    # We get the l2 norm correctly.
+    out <- cosineNorm(X, mode="l2norm")
+    ref <- cosineNorm(as.matrix(X), mode="l2norm")
+    expect_equivalent(ref, out)
 })
 
 set.seed(10002)
