b3f9fdd952652220a43edb6cb5c218afc37cb122
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-01-26 11:09:46 +0000

    Added single input tests for multiBatchPCA. Use multiBatchPCA directly in tests.

--- tests/testthat/test-multi-pca.R
+++ tests/testthat/test-multi-pca.R
@@ -13,16 +13,16 @@ test_that("multi-sample PCA works as expected", {
     test2 <- matrix(rnorm(2000), nrow=10)
 
     # Checking that we get the same result independent of the multiples of the data. 
-    ref <- batchelor:::.multi_pca(list(test1, test2), d=5)
-    out <- batchelor:::.multi_pca(list(test1, cbind(test2, test2)), d=5)
+    ref <- multiBatchPCA(test1, test2, d=5)
+    out <- multiBatchPCA(test1, cbind(test2, test2), d=5)
     expect_equal_besides_sign(ref[[1]], out[[1]])
     expect_equal_besides_sign(rbind(ref[[2]], ref[[2]]), out[[2]])
     expect_identical(ncol(ref[[1]]), 5L)
     expect_identical(ncol(ref[[2]]), 5L)
 
     # Another check. 
-    ref <- batchelor:::.multi_pca(list(test1, test2), d=3)
-    out <- batchelor:::.multi_pca(list(cbind(test1, test1, test1), test2), d=3)
+    ref <- multiBatchPCA(test1, test2, d=3)
+    out <- multiBatchPCA(cbind(test1, test1, test1), test2, d=3)
     expect_equal_besides_sign(ref[[2]], out[[2]])
     expect_equal_besides_sign(rbind(ref[[1]], ref[[1]], ref[[1]]), out[[1]])
     expect_identical(ncol(ref[[1]]), 3L)
@@ -30,39 +30,51 @@ test_that("multi-sample PCA works as expected", {
 
     # Checking with equal numbers of cells - should be equivalent to cbind'd PCA. 
     test3 <- matrix(rnorm(1000), nrow=10)
-    out <- batchelor:::.multi_pca(list(test1, test3), d=4)
+    out <- multiBatchPCA(test1, test3, d=4)
     ref <- prcomp(t(cbind(test1, test3)), rank.=4)
     expect_equal_besides_sign(rbind(out[[1]], out[[2]]), unname(ref$x))
     
     # Checking that the distances match up to the original values when we use full rank.
-    out <- batchelor:::.multi_pca(list(test1, test2), d=nrow(test1))
+    out <- multiBatchPCA(test1, test2, d=nrow(test1))
 
     everything <- rbind(t(test1), t(test2))
     ref.dist <- as.matrix(dist(everything))
     out.dist <- as.matrix(dist(rbind(out[[1]], out[[2]])))
     expect_equal(ref.dist, out.dist)
     
-    out2 <- batchelor:::.multi_pca(list(test1, cbind(test2, test2)), d=nrow(test1))
+    out2 <- multiBatchPCA(test1, cbind(test2, test2), d=nrow(test1))
     out.dist2 <- as.matrix(dist(do.call(rbind, as.list(out2))))
     everything2 <- rbind(everything, t(test2))
     ref.dist2 <- as.matrix(dist(everything2)) 
     expect_equal(out.dist2, ref.dist2)
+
+    # Throws an error on mismatched dimensions.
+    expect_error(multiBatchPCA(test1, test2[0,,drop=FALSE]), "not the same")
 })
 
-set.seed(1200001)
-test_that("multi-sample PCA works with high-level options", {
+set.seed(12000011)
+test_that("multi-sample PCA works with subsetting", {
     test1 <- matrix(rnorm(1000), nrow=10)
     test2 <- matrix(rnorm(2000), nrow=10)
 
-    # Subsetting works correctly.
     i <- sample(nrow(test1), 5)
     ref <- multiBatchPCA(test1, test2, d=3, subset.row=i)
     out <- multiBatchPCA(test1[i,], test2[i,], d=3)
     expect_equal(ref, out)
 
-    # Behaves with SCE objects as input.
+    l <- rbinom(nrow(test1), 1, 0.5)==1
+    ref <- multiBatchPCA(test1, test2, d=3, subset.row=l)
+    out <- multiBatchPCA(test1[l,], test2[l,], d=3)
+    expect_equal(ref, out) 
+})
+
+set.seed(12000012)
+test_that("multi-sample PCA works with SCEs", {
+    test1 <- matrix(rnorm(1000), nrow=8)
+    test2 <- matrix(rnorm(2000), nrow=8)
     sce1 <- SingleCellExperiment(list(logcounts=test1))
     sce2 <- SingleCellExperiment(list(logcounts=test2))
+
     ref <- multiBatchPCA(sce1, sce2, d=4)
     out <- multiBatchPCA(test1, test2, d=4)
     expect_equal(ref, out)
@@ -76,33 +88,58 @@ test_that("multi-sample PCA works with high-level options", {
     expect_equal(ref, out)
 
     # Spikes and subsetting interact correctly.
+    i <- sample(nrow(test1), 5)
     ref <- multiBatchPCA(sce1, sce2, d=2, subset.row=i)
     out <- multiBatchPCA(sce1[i,], sce2[i,], d=2)
     expect_equal(ref, out)
 
     # Throws a variety of useful errors.
     expect_error(multiBatchPCA(sce1, test2), "cannot mix")
-    expect_error(multiBatchPCA(), "at least one batch")
-    expect_error(multiBatchPCA(test1, test2[0,,drop=FALSE]), "not the same")
     sce1x <- clearSpikes(sce1)
     expect_error(multiBatchPCA(sce1x, sce2), "spike-in sets")
 })
 
 set.seed(1200002)
-test_that("multi-sample PCA works with rotation vectors", {
+test_that("multi-sample PCA rotation vector inference works", {
     test1 <- matrix(rnorm(1000), nrow=10)
     test2 <- matrix(rnorm(2000), nrow=10)
     N <- nrow(test1)
 
-    ref <- batchelor:::.multi_pca(list(test1, test2), d=5)
+    ref <- multiBatchPCA(test1, test2, d=5)
     others <- c(7, 3, 1, 2)
     expanded <- c(seq_len(N), others)
-    out <- batchelor:::.multi_pca(list(test1[expanded,], test2[expanded,]), d=5, subset.row=1:N, rotate.all=TRUE)
+    out <- multiBatchPCA(test1[expanded,], test2[expanded,], d=5, subset.row=1:N, rotate.all=TRUE)
 
     expect_equal(ref[[1]], out[[1]])
     expect_equal(ref[[2]], out[[2]])
-    expect_equal(metadata(ref)$rotation, metadata(out)$rotation[1:N,])
-    expect_equal(metadata(ref)$rotation[others,], metadata(out)$rotation[N + seq_along(others),])
+    expect_equal(metadata(ref)$rotation[expanded,], metadata(out)$rotation)
+
+    # Also works with a logical vector for subsetting. 
+    alt <- multiBatchPCA(test1[expanded,], test2[expanded,], d=5, subset.row=seq_along(expanded) <= N, rotate.all=TRUE)
+    expect_equal(alt, out)
+})
+
+set.seed(12000021)
+test_that("multi-sample PCA works with a single input", {
+    test1 <- matrix(rnorm(1000), nrow=10)
+    test2 <- matrix(rnorm(2000), nrow=10)
+    ref <- multiBatchPCA(test1, test2, d=5)
+
+    combined <- cbind(test1, test2)
+    batch <- rep(1:2, c(ncol(test1), ncol(test2)))
+    out <- multiBatchPCA(combined, batch=batch, d=5)
+    expect_equal(rbind(ref[[1]], ref[[2]]), out[[1]])
+    expect_equal(metadata(out), metadata(ref))
+
+    # Handles reordering.
+    o <- sample(length(batch))
+    alt <- multiBatchPCA(combined[,o,drop=FALSE], batch=batch[o], d=5)
+    expect_equal(out[[1]][o,], alt[[1]])
+    expect_equal(metadata(out), metadata(alt))
+
+    # Expected errors.
+    expect_error(multiBatchPCA(), "at least one batch")
+    expect_error(multiBatchPCA(test1), "'batch' must be specified")
 })
 
 set.seed(1200003)
