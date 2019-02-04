72c5f1d82cb006c7b85702f5c5a992b604f1643b
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-02-04 09:30:30 +0000

    Tested subsetting utility functions.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -125,3 +125,80 @@ test_that(".create_batch_names works correctly", {
     out <- batchelor:::.create_batch_names(NULL, c(10, 20, 30))
     expect_identical(as.integer(out$ids), rep(out$labels, c(10, 20, 30)))
 })
+
+set.seed(1000006)
+test_that(".row_subset_to_index works correctly", {
+    A <- matrix(runif(1000), ncol=1)
+    rownames(A) <- paste0("GENE_", seq_along(A))
+
+    i <- sample(nrow(A), nrow(A)/2)
+    expect_identical(i, batchelor:::.row_subset_to_index(A, i))
+
+    l <- rbinom(nrow(A), 1, 0.1)==1
+    expect_identical(which(l), batchelor:::.row_subset_to_index(A, l))
+
+    s <- sample(rownames(A), nrow(A)/2)
+    expect_identical(match(s, rownames(A)), batchelor:::.row_subset_to_index(A, s))
+
+    expect_identical(batchelor:::.row_subset_to_index(A, NULL), seq_along(A))
+
+    # Testing silly inputs.
+    expect_identical(batchelor:::.row_subset_to_index(A, integer(0)), integer(0))
+    expect_identical(batchelor:::.row_subset_to_index(A, logical(0)), integer(0))
+    expect_identical(batchelor:::.row_subset_to_index(A, character(0)), integer(0))
+})
+
+set.seed(1000007)
+test_that(".spike_subset works correctly", {
+    sce <- SingleCellExperiment(list(logcounts=matrix(runif(2000), nrow=10)))
+
+    # Behaves without any spike-ins.
+    expect_identical(batchelor:::.spike_subset(sce, TRUE), NULL)
+    expect_identical(batchelor:::.spike_subset(sce, FALSE), NULL)
+
+    # Behaves once you throw spike-ins in.
+    isSpike(sce, "ERCC") <- 1:5
+    expect_identical(batchelor:::.spike_subset(sce, TRUE), NULL)
+    expect_identical(batchelor:::.spike_subset(sce, FALSE), !isSpike(sce))
+
+    # Testing silly inputs.
+    expect_identical(batchelor:::.spike_subset(sce[0,], TRUE), NULL)
+    expect_identical(batchelor:::.spike_subset(sce[0,], FALSE), NULL)
+})
+
+set.seed(1000008)
+test_that(".SCE_subset_genes works correctly", {
+    sce <- SingleCellExperiment(list(logcounts=matrix(runif(2000), nrow=10)))
+
+    # Behaves without any spike-ins.
+    expect_identical(batchelor:::.SCE_subset_genes(NULL, sce, TRUE), NULL)
+    expect_identical(batchelor:::.SCE_subset_genes(NULL, sce, FALSE), NULL)
+
+    i <- 1:5
+    expect_identical(batchelor:::.SCE_subset_genes(i, sce, TRUE), i)
+    expect_identical(batchelor:::.SCE_subset_genes(i, sce, FALSE), i)
+
+    l <- seq_len(nrow(sce)) %% 2 == 0
+    expect_identical(batchelor:::.SCE_subset_genes(l, sce, TRUE), which(l))
+    expect_identical(batchelor:::.SCE_subset_genes(l, sce, FALSE), which(l))
+
+    # Behaves once you throw spike-ins in.
+    isSpike(sce, "ERCC") <- 1:5
+    expect_identical(batchelor:::.SCE_subset_genes(NULL, sce, TRUE), NULL)
+    expect_identical(batchelor:::.SCE_subset_genes(NULL, sce, FALSE), !isSpike(sce))
+
+    i <- 3:10
+    expect_identical(batchelor:::.SCE_subset_genes(i, sce, TRUE), i)
+    expect_identical(batchelor:::.SCE_subset_genes(i, sce, FALSE), 6:10)
+
+    l <- seq_len(nrow(sce)) %% 2 == 0
+    expect_identical(batchelor:::.SCE_subset_genes(l, sce, TRUE), which(l))
+    expect_identical(batchelor:::.SCE_subset_genes(l, sce, FALSE), setdiff(which(l), 1:5))
+
+    # Testing silly inputs.
+    expect_identical(batchelor:::.SCE_subset_genes(NULL, sce[0,], TRUE), NULL)
+    expect_identical(batchelor:::.SCE_subset_genes(NULL, sce[0,], FALSE), NULL)
+
+    expect_identical(batchelor:::.SCE_subset_genes(integer(0), sce[0,], TRUE), integer(0))
+    expect_identical(batchelor:::.SCE_subset_genes(integer(0), sce[0,], FALSE), integer(0))
+})
