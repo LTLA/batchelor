0a0c1914d6b4db540eef0ab0f61c0b7dd1dad847
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-08-17 12:06:40 -0700

    Simplified multiBatchNorm to simply ignore spike-ins.

--- R/multiBatchNorm.R
+++ R/multiBatchNorm.R
@@ -17,30 +17,26 @@
 #' This is roughly equivalent to the between-cluster normalization described by Lun et al. (2016).
 #' 
 #' This function will adjust the size factors so that counts in high-coverage batches are scaled \emph{downwards} to match the coverage of the most shallow batch.
-#' The \code{\link[scater]{normalize}} function will then add the same pseudo-count to all batches before log-transformation.
+#' The \code{\link{logNormCounts}} function will then add the same pseudo-count to all batches before log-transformation.
 #' By scaling downwards, we favour stronger squeezing of log-fold changes from the pseudo-count, mitigating any technical differences in variance between batches.
 #' Of course, genuine biological differences will also be shrunk, but this is less of an issue for upregulated genes with large counts.
 #' 
-#' This function is preferred over running \code{\link{normalize}} directly when computing log-normalized values for use in \code{\link{mnnCorrect}} or \code{\link{fastMNN}}.
+#' Using \code{multiBatchNorm} may be better than running \code{\link{logNormCounts}} separately for each batch prior to correction.
 #' In most cases, size factors will be computed within each batch;
-#' their direct application in \code{\link{normalize}} will not account for scaling differences between batches.
+#' batch-specific application in \code{\link{logNormCounts}} will not account for scaling differences between batches.
 #' In contrast, \code{multiBatchNorm} will rescale the size factors so that they are comparable across batches.
 #' 
 #' Only genes with library size-adjusted average counts greater than \code{min.mean} will be used for computing the rescaling factors.
 #' This improves precision and avoids problems with discreteness.
 #' Users can also set \code{subset.row} to restrict the set of genes used for computing the rescaling factors.
 #' However, this only affects the rescaling of the size factors - normalized values for \emph{all} genes will still be returned.
-#' 
-#' @section Handling spike-ins:
-#' Spike-in transcripts should be either absent in all batches or, if present, they should be the same across all batches.
-#' Rows annotated as spike-in transcripts are not used to compute the rescaling factors for endogenous genes.
-#' 
-#' By default, the spike-in size factors are rescaled using the same scaling factor for the endogenous genes in the same batch.
-#' This preserves the abundances of the spike-in transcripts relative to the endogenous genes, 
-#' which is important if the returned SingleCellExperiments are to be used to model technical noise.
-#' 
-#' If \code{separate.spikes=TRUE}, spike-in size factors are rescaled separately from those of the endogenous genes.
-#' This will eliminate differences in spike-in quantities across batches at the cost of losing the ability to compare between endogenous and spike-in transcripts within each batch.
+#'
+#' @section Note about spike-ins:
+#' Rescaling is only performed on endogenous genes in each SingleCellExperiment object.
+#' If any spike-in transcripts are present in the \code{\link{altExps}},
+#' their abundances will not be rescaled here, and are no longer directly comparable to the rescaled abundances of the genes.
+#' This is usually not a major problem as spike-ins are rarely used during the batch correction itself -
+#' however, users should not attempt to perform variance modelling with the spike-ins on the output of this function.
 #' 
 #' @return
 #' A list of SingleCellExperiment objects with normalized log-expression values in the \code{"logcounts"} assay (depending on values in \code{norm.args}).
@@ -49,7 +45,7 @@
 #' Aaron Lun
 #' 
 #' @seealso
-#' \code{\link{mnnCorrect}} and \code{\link{fastMNN}} for methods that can benefit from rescaling.
+#' \code{\link{mnnCorrect}} and \code{\link{fastMNN}}, for methods that can benefit from rescaling.
 #'
 #' \code{\link[scater]{normalize}} for the calculation of log-transformed normalized expression values.
 #' 
@@ -67,112 +63,54 @@
 #' summary(sizeFactors(out[[2]]))
 #' 
 #' @references
-#' Lun ATL, Bach K and Marioni JC (2016).
-#' Pooling across cells to normalize single-cell RNA sequencing data with many zero counts.
-#' \emph{Genome Biol.} 17:75
+#' Lun ATL (2018).
+#' Further MNN algorithm development.
+#' \url{https://MarioniLab.github.io/FurtherMNN2018/theory/description.html}
 #'
 #' @export
-#' @importFrom BiocGenerics normalize
-#' @importMethodsFrom scater normalize
-#' @importFrom SingleCellExperiment isSpike spikeNames
-multiBatchNorm <- function(..., assay.type="counts", norm.args=list(), min.mean=1, subset.row=NULL, separate.spikes=FALSE)
-# Performs multi-batch normalization, adjusting for differences 
-# in scale between SCE objects supplied in '...'.
-# 
-# written by Aaron Lun
-# created 4 June 2018
-{
+#' @importFrom BiocGenerics sizeFactors sizeFactors<-
+#' @importMethodsFrom scater logNormCounts librarySizeFactors
+multiBatchNorm <- function(..., assay.type="counts", norm.args=list(), min.mean=1, subset.row=NULL) {
     batches <- list(...)
     checkBatchConsistency(batches)
     if (length(batches)==0L) { 
         stop("at least one SingleCellExperiment must be supplied") 
     }
-    checkSpikeConsistency(batches)
 
-    # Centering the endogenous size factors, and rescaling all spike-in size factors for the ride.
+    # Centering the endogenous size factors.
     for (b in seq_along(batches)) {
         current <- batches[[b]]
 
         cursf <- sizeFactors(current)
         if (is.null(cursf)) {
             cursf <- librarySizeFactors(current, exprs_values=assay.type, subset_row=subset.row)
-            warning(sprintf("no endogenous size factors in batch %i, using library sizes instead", b))
         }
 
         centering <- mean(cursf)
         sizeFactors(current) <- cursf / centering
-
-        for (spike in spikeNames(current)) {
-            spsf <- sizeFactors(current, spike)
-            if (is.null(spsf)) {
-                spsf <- librarySizeFactors(current, exprs_values=assay.type, subset_row=isSpike(current, spike))
-                warning(sprintf("no %s size factors in batch %i, using library sizes instead", spike, b))
-            }
-
-            # If we want to rescale spike-ins separately, they'll need to be centered as well.
-            if (separate.spikes) {
-                spsf <- spsf/mean(spsf)
-            } else {
-                spsf <- spsf/centering
-            }
-
-            sizeFactors(current, spike) <- spsf
-        }
-
         batches[[b]] <- current
     }
 
-    # Adjusting size factors for the non-spike-in transcripts.
+    # Adjusting size factors.
     nonspike.subset <- .SCE_subset_genes(subset.row, batches[[1]], get.spikes=FALSE)
-    if (is.null(nonspike.subset) || any(nonspike.subset)) { # works for logical or integer
-        endog.rescale <- .compute_batch_rescaling(batches, subset.row=nonspike.subset, assay.type=assay.type, min.mean=min.mean)
-        batches <- .rescale_size_factors(batches, endog.rescale)
-    } else {
-        endog.rescale <- rep(1, length(batches))
-    }
-
-    # Adjusting size factors for each of the spike-ins.
-    ref.spike.names <- spikeNames(batches[[1]])
-    subset.row <- .row_subset_to_index(batches[[1]], subset.row)
-
-    for (spike in ref.spike.names) {
-        ref.spike <- isSpike(batches[[1]], spike)
-        for (b in seq_along(batches)) {
-            if (!identical(ref.spike, isSpike(batches[[b]], spike))) {
-                stop(sprintf("%s spike-in identities differ across batches", spike))
-            }
-        }
-
-        if (separate.spikes) {
-            spike.subset <- intersect(which(ref.spike), subset.row)
-            if (length(spike.subset)) { 
-                spike.rescale <- .compute_batch_rescaling(batches, subset.row=spike.subset, 
-                    assay.type=assay.type, sf.type=spike, min.mean=min.mean)
-            }
-        } else {
-            spike.rescale <- endog.rescale
-        }
-        batches <- .rescale_size_factors(batches, spike.rescale, sf.type=spike)
-    }
+    endog.rescale <- .compute_batch_rescaling(batches, subset.row=nonspike.subset, assay.type=assay.type, min.mean=min.mean)
+    batches <- .rescale_size_factors(batches, endog.rescale)
 
     # Applying the normalization.   
-    mapply(FUN=normalize, batches, MoreArgs=c(list(exprs_values=assay.type, centre_size_factors=FALSE), norm.args), SIMPLIFY=FALSE)
+    mapply(FUN=logNormCounts, batches, 
+        MoreArgs=c(list(exprs_values=assay.type, center_size_factors=FALSE), norm.args), 
+        SIMPLIFY=FALSE)
 }
 
-#' @importFrom scater calcAverage 
+#' @importFrom scater calculateAverage 
 #' @importFrom stats median
-#' @importFrom BiocGenerics sizeFactors 
-.compute_batch_rescaling <- function(batches, subset.row, assay.type, min.mean, sf.type=NULL)
+#' @importFrom BiocGenerics sizeFactors sizeFactors<-
+.compute_batch_rescaling <- function(batches, subset.row, assay.type, min.mean) 
 # Computes the median ratios (a la DESeq normalization),
 # finds the smallest ratio and uses that as the reference.
 {
     nbatches <- length(batches)
-    collected.ave <- vector("list", nbatches)
-
-    for (b in seq_along(batches)) {
-        collected.ave[[b]] <- calcAverage(assay(batches[[b]], assay.type, withDimnames=FALSE), 
-            subset_row=subset.row, use_size_factors=sizeFactors(batches[[b]], sf.type))
-     }
+    collected.ave <- lapply(batches, FUN=calculateAverage, exprs_values=assay.type, subset_row=subset.row)
 
     collected.ratios <- matrix(1, nbatches, nbatches)
     for (first in seq_len(nbatches-1L)) {
@@ -203,17 +141,16 @@ multiBatchNorm <- function(..., assay.type="counts", norm.args=list(), min.mean=
 }
 
 #' @importFrom BiocGenerics sizeFactors sizeFactors<-
-#' @importFrom scater librarySizeFactors
-.rescale_size_factors <- function(batches, rescaling, assay.type, sf.type=NULL) 
+.rescale_size_factors <- function(batches, rescaling, assay.type)
 # Applying the chosen rescaling.
 {
     for (idx in seq_along(batches)) {
         current <- batches[[idx]]
-        cursf <- sizeFactors(current, sf.type)
+        cursf <- sizeFactors(current)
 
         # Adjusting the size factors for the new reference.
         cursf <- cursf / rescaling[idx] 
-        sizeFactors(current, sf.type) <- cursf
+        sizeFactors(current) <- cursf
         batches[[idx]] <- current
     }
 
--- tests/testthat/test-multi-norm.R
+++ tests/testthat/test-multi-norm.R
@@ -29,14 +29,14 @@ test_that("multiBatchNorm works properly", {
     re.out <- multiBatchNorm(X3, X, X2)
     expect_equal(out[c(3,1,2)], re.out)
 
-    # Single batch input just returns the same object as normalize().
+    # Single batch input just returns the same object as logNormCounts().
     solo.out <- multiBatchNorm(X3)
-    expect_equal(solo.out[[1]], normalize(X3))
+    expect_equal(solo.out[[1]], scater::logNormCounts(X3))
 
     # Reverts to the library size correctly.
     Xtmp <- X
     sizeFactors(Xtmp) <- NULL
-    expect_warning(out <- multiBatchNorm(Xtmp, X2), "no endogenous size factors in batch 1") 
+    out <- multiBatchNorm(Xtmp)
     expect_equal(sizeFactors(out[[1]]), scater::librarySizeFactors(Xtmp))
 })
 
@@ -58,7 +58,7 @@ test_that("multiBatchNorm size factor centering logic is correct", {
     expect_equal(mean(sizeFactors(out[[2]])), M)
     expect_equal(sizeFactors(scater::centreSizeFactors(X2)), sizeFactors(out[[2]]) / M)
 
-    # Renormalized values have no composition biases.
+    # RelogNormCountsd values have no composition biases.
     ave1 <- scater::calcAverage(out[[1]])
     ave2 <- scater::calcAverage(out[[2]]) / M # as calcAverage automatically centers out[[2]'s SFs.
     expect_equal(1, median(ave2/ave1))
@@ -112,140 +112,3 @@ test_that("multiBatchNorm behaves correctly with gene filtering", {
     expect_equal(logcounts(sub.out[[1]])[randoms,], logcounts(ref.out[[1]])) 
     expect_equal(logcounts(sub.out[[2]])[randoms,], logcounts(ref.out[[2]])) 
 })
-
-set.seed(200121)
-test_that("multiBatchNorm rescales spike-ins correctly", {
-    isp <- rbinom(ngenes, 1, 0.1)==1
-    isSpike(X, "ERCC") <- isp
-    sizeFactors(X, "ERCC") <- runif(ncol(X))
-
-    X2 <- X
-    counts(X2) <- counts(X2) * 2L
-    X3 <- X
-    counts(X3) <- counts(X3) * 3L
-
-    out <- multiBatchNorm(X, X2, X3)
-
-    M <- mean(sizeFactors(X))
-    expect_equal(sizeFactors(out[[1]]), sizeFactors(X)/M)
-    expect_equal(sizeFactors(out[[1]]), sizeFactors(out[[2]]) / 2)
-    expect_equal(sizeFactors(out[[1]]), sizeFactors(out[[3]]) / 3)
-
-    expect_equal(sizeFactors(out[[1]], "ERCC"), sizeFactors(X, "ERCC")/M)
-    expect_equal(sizeFactors(out[[1]], "ERCC"), sizeFactors(out[[2]], "ERCC")/2)
-    expect_equal(sizeFactors(out[[1]], "ERCC"), sizeFactors(out[[3]], "ERCC")/3)
-    
-    # Missing spike-in size factors are handled correctly.
-    Xtmp <- X
-    sizeFactors(Xtmp, "ERCC") <- NULL
-    expect_warning(mout <- multiBatchNorm(Xtmp, X2), "no ERCC size factors")
-    expect_equal(sizeFactors(mout[[1]], "ERCC"), scater::librarySizeFactors(X, subset_row=isp)/M)
-    expect_equal(mout[[2]], out[[2]])
-
-    # Does nothing by default, when there are no endogenous genes.
-    nout <- multiBatchNorm(X[isp,], X2[isp,], X3[isp,])
-
-    expect_equal(sizeFactors(nout[[1]]), sizeFactors(X)/M)
-    expect_equal(sizeFactors(nout[[1]]), sizeFactors(nout[[2]]))
-    expect_equal(sizeFactors(nout[[1]]), sizeFactors(nout[[3]]))
-
-    expect_equal(sizeFactors(nout[[1]], "ERCC"), sizeFactors(X, "ERCC")/M)
-    expect_equal(sizeFactors(nout[[1]], "ERCC"), sizeFactors(nout[[2]], "ERCC"))
-    expect_equal(sizeFactors(nout[[1]], "ERCC"), sizeFactors(nout[[3]], "ERCC"))
-})
-
-set.seed(200121)
-test_that("multiBatchNorm handles separate rescaling of spike-ins correctly", {
-    dummy2 <- matrix(rnbinom(ngenes*ncells, mu=means, size=5), ncol=ncells, nrow=ngenes)
-    rownames(dummy2) <- rownames(dummy)
-    X2 <- SingleCellExperiment(list(counts=dummy2))
-    sizeFactors(X2) <- runif(ncol(X2))
-
-    Y <- X
-    isp <- rbinom(ngenes, 1, 0.1)==1
-    isSpike(Y, "ERCC") <- isp
-    sizeFactors(Y, "ERCC") <- runif(ncol(X))
-
-    Y2 <- X2
-    isSpike(Y2, "ERCC") <- isp
-    sizeFactors(Y2, "ERCC") <- runif(ncol(X2))
-
-    ref <- multiBatchNorm(Y, Y2, separate.spikes=TRUE)
-
-    # Non-spike-in counts are computed correctly.
-    out <- multiBatchNorm(X[!isp,], X2[!isp,])
-    expect_equal(logcounts(ref[[1]])[!isp,], logcounts(out[[1]]))
-    expect_equal(logcounts(ref[[2]])[!isp,], logcounts(out[[2]]))
-
-    # Spike-in counts are computed correctly.
-    alt <- X[isp,]
-    sizeFactors(alt) <- sizeFactors(Y, "ERCC")
-    alt2 <- X2[isp,]
-    sizeFactors(alt2) <- sizeFactors(Y2, "ERCC")
-
-    sout <- multiBatchNorm(alt, alt2)
-    expect_equal(sizeFactors(ref[[1]], "ERCC"), sizeFactors(sout[[1]])) 
-    expect_equal(sizeFactors(ref[[2]], "ERCC"), sizeFactors(sout[[2]])) 
-    expect_equal(logcounts(ref[[1]])[isp,], logcounts(sout[[1]])) 
-    expect_equal(logcounts(ref[[2]])[isp,], logcounts(sout[[2]])) 
-
-    # Missing spike-in size factors are handled correctly.
-    Ytmp <- Y
-    sizeFactors(Ytmp, "ERCC") <- NULL
-    expect_warning(nout <- multiBatchNorm(Ytmp, Y2, separate.spikes=TRUE), "no ERCC size factors")
-    expect_equal(sizeFactors(nout[[1]]), sizeFactors(out[[1]]))
-    expect_equal(sizeFactors(nout[[2]]), sizeFactors(out[[2]]))
-
-    alttmp <- X[isp,]
-    sizeFactors(alttmp) <- NULL
-    expect_warning(comp <- multiBatchNorm(alttmp, alt2), "no endogenous size factors")
-    expect_equal(sizeFactors(comp[[1]]), sizeFactors(nout[[1]], "ERCC"))
-    expect_equal(sizeFactors(comp[[2]]), sizeFactors(nout[[2]], "ERCC"))
-
-    # Spike-in selection and subset.row interact correctly.
-    randoms <- sample(ngenes, 500)
-    ref <- multiBatchNorm(Y[randoms,], Y2[randoms,])
-    out <- multiBatchNorm(Y, Y2, subset.row=randoms)
-    expect_equal(logcounts(ref[[1]]), logcounts(out[[1]])[randoms,])
-    expect_equal(logcounts(ref[[2]]), logcounts(out[[2]])[randoms,])
-})
-
-set.seed(20013)
-test_that("multiBatchNorm spits the dummy correctly", {
-    # Mismatching row names or dimensions.
-    X2 <- X
-    rownames(X2) <- sample(rownames(X2))
-    expect_error(out <- multiBatchNorm(X, X2), "not the same")
-    rownames(X2) <- NULL
-    expect_error(out <- multiBatchNorm(X, X2), "not the same")
-
-    X2 <- X
-    expect_error(out <- multiBatchNorm(X, X2[1,]), "not the same")
-
-    # Mismatching spike-in identities.
-    Xsp <- X
-    isSpike(Xsp, "ERCC") <- sample(ngenes, 200)
-    expect_error(multiBatchNorm(Xsp, X2), "spike-in sets differ across batches")
-    X2sp <- X
-    isSpike(X2sp, "ERCC") <- sample(ngenes, 200)
-    expect_error(multiBatchNorm(Xsp, X2sp), "spike-in identities differ across batches")
-
-    Xtmp <- Xsp
-    isSpike(Xtmp, "SIRV") <- sample(ngenes, 50)
-    X2tmp <- X
-    isSpike(X2tmp, "ERCC") <- isSpike(Xtmp, "SIRV") 
-    isSpike(X2tmp, "SIRV") <- isSpike(Xtmp, "ERCC") 
-    expect_error(multiBatchNorm(Xtmp, X2tmp), "ERCC spike-in identities differ across batches")
-
-    # Input resulting in NA scaling values (add 1 to avoid NA size factors).
-    X2 <- X
-    counts(X2)[] <- 0
-    counts(X2)[1,] <- 1
-    expect_error(out <- multiBatchNorm(X, X2), "not finite")
-
-    # Empty inputs.
-    expect_error(out <- multiBatchNorm(X[0,], X2[0,]), "not finite")
-    expect_error(out <- multiBatchNorm(X[,0], X2[,0]), "not finite")
-    expect_error(multiBatchNorm(), "at least one SingleCellExperiment")
-})
-
